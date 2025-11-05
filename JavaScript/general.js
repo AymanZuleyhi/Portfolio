e2962996462c8d9a66eee0c7366d6ea964cf0ffc
AymanZuleyhi <149739736+AymanZuleyhi@users.noreply.github.com>
2025-11-05 09:40:39 +0200

    changes

--- JavaScript/general.js
+++ JavaScript/general.js
@@ -51,8 +51,9 @@ const listOfTechnologies = [
 
 const listOfProjects = [
   {
-    video: "./Files/Projects/Pokedex/Video.webm",
-    logo: "./Files/Projects/Pokedex/Pokedex.png",
+    video:
+      "https://res.cloudinary.com/dhzq0yjf2/video/upload/v1762327449/Video_ru3xsr.webm",
+    logo: "https://res.cloudinary.com/dhzq0yjf2/image/upload/v1762327447/Pokedex_ext9gr.png",
     h1: "Pokedex",
     description:
       "A modern, user-friendly Pokédex that lets users explore and search for Pokémon, view detailed stats, download images and information, and even save their favorite Pokémon for quick access.",
@@ -66,8 +67,9 @@ const listOfProjects = [
     },
   },
   {
-    video: "./Files/Projects/Crunchy/Video.webm",
-    logo: "./Files/Projects/Crunchy/Crunchy.png",
+    video:
+      "https://res.cloudinary.com/dhzq0yjf2/video/upload/v1762327433/Video_fbtdfg.webm",
+    logo: "https://res.cloudinary.com/dhzq0yjf2/image/upload/v1762327431/Crunchy_bzks90.png",
     h1: "Crunchy",
     description:
       "A full-stack recipe application where users can create accounts, upload their favorite recipes, and easily filter through a diverse collection to find new culinary ideas.",
@@ -89,8 +91,9 @@ const listOfProjects = [
     },
   },
   {
-    video: "./Files/Projects/AdelineSportswear/Video.webm",
-    logo: "./Files/Projects/AdelineSportswear/AdelineSportswear.png",
+    video:
+      "https://res.cloudinary.com/dhzq0yjf2/video/upload/v1762327339/Video_sixz6y.webm",
+    logo: "https://res.cloudinary.com/dhzq0yjf2/image/upload/v1762327336/AdelineSportswear_lwoqgd.png",
     h1: "Adeline Sportswear",
     description:
       "A clothing brand focused on activewear. This website makes it easy for customers to browse, find, and buy sportswear items, with email marketing managed through Klaviyo.",
@@ -100,8 +103,9 @@ const listOfProjects = [
     },
   },
   {
-    video: "./Files/Projects/Adhominem/Video.webm",
-    logo: "./Files/Projects/Adhominem/Adhominem.png",
+    video:
+      "https://res.cloudinary.com/dhzq0yjf2/video/upload/v1762327376/Video_beby8v.webm",
+    logo: "https://res.cloudinary.com/dhzq0yjf2/image/upload/v1762327374/Adhominem_p4fufb.png",
     h1: "Adhominem",
     description:
       "Developed a custom search bar to enable users to easily filter and browse through all available products on the site.",
@@ -112,8 +116,9 @@ const listOfProjects = [
     },
   },
   {
-    video: "./Files/Projects/BeatGrabber/Video.webm",
-    logo: "./Files/Projects/BeatGrabber/BeatGrabber.png",
+    video:
+      "https://res.cloudinary.com/dhzq0yjf2/video/upload/v1762327418/Video_lnbgrl.webm",
+    logo: "https://res.cloudinary.com/dhzq0yjf2/image/upload/v1762327413/BeatGrabber_wsdi3e.png",
     h1: "BeatGrabber",
     description:
       "Developed a tool to convert audio from YouTube and SoundCloud to MP3 format, allowing for quick and easy personal downloads. The tool performs direct, API-free conversions using FFmpeg and yt-dlp.",
